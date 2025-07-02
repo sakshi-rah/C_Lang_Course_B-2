5c9980883cbff777a42aea4cbf3bdc63c5e343a0
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-07-02 21:22:45 +0530

    Day-1 Introduction of C

---
+++ DAY-1 (Intro)/intro_c.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // header file std I\O
+
+
+// return_type function_name(){} 1. int = something value return, void = nothing to need return anything
+int main(){
+
+    printf("Hello, C Learners! Nice to see you.\n");
+    printf("Name: Sakshi Rahangdale\n");
+    printf("Age: 26\n");
+    printf("Position: C Programmer\n");
+
+}
