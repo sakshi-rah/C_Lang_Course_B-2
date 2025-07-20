d9c5dc375c19301fdfa372a06958fc164d7e5cc3
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-07-20 11:51:32 +0530

    DAY-3 & DAY-8 Updated

--- DAY-8 (Switch Case)/switchCase.c
+++ DAY-8 (Switch Case)/switchCase.c
@@ -38,26 +38,46 @@ switch(expression/value){
 // } 
 
 // Week Days
-int day;
-printf("Enter Day: ");
-scanf(" %d", &day);
+// int day;
+// printf("Enter Day: ");
+// scanf(" %d", &day);
 
-switch(day){
-    case 1: printf("Sunday");
+// switch(day){
+//     case 1: printf("Sunday");
+//     break;
+//     case 2: printf("Monday");
+//     break;
+//     case 3: printf("Tuesday");
+//     break;
+//     case 4: printf("Wednesday");
+//     break;
+//     case 5: printf("Thursday");
+//     break;
+//     case 6: printf("Friday");
+//     break;
+//     case 7: printf("Saturday");
+//     break;
+//     default: printf("Invalid day number");
+// }
+
+
+// switch case another example:
+
+int colorNo;
+printf("Enter your color no.: ");
+scanf("%d", &colorNo);
+
+switch(colorNo){
+    case 1: printf("1: Pink");
     break;
-    case 2: printf("Monday");
+    case 2: printf("2: Black");
     break;
-    case 3: printf("Tuesday");
+    case 3: printf("3: Blue");
     break;
-    case 4: printf("Wednesday");
+    case 4: printf("4: Brown");
     break;
-    case 5: printf("Thursday");
-    break;
-    case 6: printf("Friday");
-    break;
-    case 7: printf("Saturday");
-    break;
-    default: printf("Invalid day number");
+    default: printf("Invalid color number");
 }
+
     return 0;
 }
